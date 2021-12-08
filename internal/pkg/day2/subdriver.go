d912d53919192230be57f2ba79a6b49f6feed325
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2021-12-08 11:08:41 -0600

    day 2 part 1 solution

---
+++ internal/pkg/day2/subdriver.go
@@ -0,0 +1,69 @@
+package day2
+
+import "errors"
+
+type HelmOrder struct {
+	Command string
+	Units   int
+}
+
+func MakeHelmOrder(command string, units int) HelmOrder {
+	return HelmOrder{
+		command,
+		units,
+	}
+}
+
+type Submarine struct {
+	horizontalPostiion int
+	depth              int
+}
+
+func NewSubmarine() *Submarine {
+	sub := new(Submarine)
+	sub.horizontalPostiion = 0
+	sub.depth = 0
+
+	return sub
+}
+
+func (sub *Submarine) moveForward(distance int) {
+	sub.horizontalPostiion += distance
+}
+
+func (sub *Submarine) descend(depth int) {
+	sub.depth += depth
+}
+
+func (sub *Submarine) ascend(depth int) {
+	sub.depth -= depth
+}
+
+func (sub Submarine) CurrentPosition() int {
+	return sub.depth * sub.horizontalPostiion
+}
+
+func (sub *Submarine) RelayOrdersToHelmsman(orders []HelmOrder) error {
+	for _, helmOrder := range orders {
+		err := sub.handleOrder(helmOrder)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (sub *Submarine) handleOrder(order HelmOrder) error {
+	switch order.Command {
+	case "forward":
+		sub.moveForward(order.Units)
+	case "up":
+		sub.ascend(order.Units)
+	case "down":
+		sub.descend(order.Units)
+	default:
+		return errors.New("received unknown command")
+	}
+	return nil
+}
--- internal/pkg/day2/subdriver_test.go
+++ internal/pkg/day2/subdriver_test.go
@@ -1,22 +1,79 @@
 package day2
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 
+	"github.com/bstauff/adventofcode21/internal/pkg/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDriveSubGiveGoodPosition(t *testing.T) {
+func TestRelayOrdersToHelmsman(t *testing.T) {
 	sub := NewSubmarine()
 
-	sub.MoveForward(5)
-	sub.Descend(5)
-	sub.MoveForward(5)
-	sub.Ascend(3)
-	sub.Descend(8)
-	sub.MoveForward(2)
+	ordersForHelm := []HelmOrder{
+		MakeHelmOrder("forward", 5),
+		MakeHelmOrder("down", 5),
+		MakeHelmOrder("forward", 8),
+		MakeHelmOrder("up", 3),
+		MakeHelmOrder("down", 8),
+		MakeHelmOrder("forward", 2),
+	}
+
+	err := sub.RelayOrdersToHelmsman(ordersForHelm)
+
+	assert.Nil(t, err)
 
 	expectedPosition := 150
 
-	assert.Equal(expectedPosition, sub.CurrentPosition())
+	assert.Equal(t, expectedPosition, sub.CurrentPosition())
+}
+
+func TestRelayOrdersToHelmsmanWithBadCommand(t *testing.T) {
+	sub := NewSubmarine()
+
+	ordersForHelm := []HelmOrder{
+		MakeHelmOrder("bananas", 5),
+	}
+
+	err := sub.RelayOrdersToHelmsman(ordersForHelm)
+
+	assert.NotNil(t, err)
+}
+
+func TestRelayOrdersToHelmsmanWithLargeInput(t *testing.T) {
+
+	transformFunc := func(s string) (interface{}, error) {
+		split := strings.Split(s, " ")
+		command := split[0]
+		units, err := strconv.Atoi(split[1])
+		if err != nil {
+			return nil, err
+		}
+		return MakeHelmOrder(command, units), nil
+	}
+
+	input, err := helpers.ReadInput("input.txt", transformFunc)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	orders := make([]HelmOrder, 0, len(input))
+
+	for _, inputData := range input {
+		order, ok := inputData.(HelmOrder)
+		if !ok {
+			t.Error("failed to get HelmOrder from input")
+		}
+		orders = append(orders, order)
+	}
+
+	sub := NewSubmarine()
+	sub.RelayOrdersToHelmsman(orders)
+
+	result := sub.CurrentPosition()
+
+	assert.Equal(t, 1654760, result)
 }
