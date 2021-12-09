cb4c878bac4571214263959b80352437388f37d1
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2021-12-09 15:05:22 -0600

    solved day 2 part 2

--- internal/pkg/day2/subwithaim_test.go
+++ internal/pkg/day2/subwithaim_test.go
@@ -1,8 +1,11 @@
 package day2
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 
+	"github.com/bstauff/adventofcode21/internal/pkg/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,4 +28,51 @@ func TestRelayOrdersToHelmsmanWithAim(t *testing.T) {
 	expectedPosition := 900
 
 	assert.Equal(t, expectedPosition, sub.CurrentPosition())
+}
+
+func TestRelayOrdersToHelmsmanWithAimWithBadCommand(t *testing.T) {
+	sub := NewAimSubmarine()
+
+	ordersForHelm := []HelmOrder{
+		MakeHelmOrder("bananas", 5),
+	}
+
+	err := sub.RelayOrdersToHelmsman(ordersForHelm)
+
+	assert.NotNil(t, err)
+}
+
+func TestRelayOrdersToHelmsmanWithAimWithLargeInput(t *testing.T) {
+
+	transformFunc := func(s string) (interface{}, error) {
+		split := strings.Split(s, " ")
+		command := split[0]
+		units, err := strconv.Atoi(split[1])
+		if err != nil {
+			return nil, err
+		}
+		return MakeHelmOrder(command, units), nil
+	}
+
+	input, err := helpers.ReadInput("input.txt", transformFunc)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	orders := make([]HelmOrder, 0, len(input))
+
+	for _, inputData := range input {
+		order, ok := inputData.(HelmOrder)
+		if !ok {
+			t.Error("failed to get HelmOrder from input")
+		}
+		orders = append(orders, order)
+	}
+
+	sub := NewAimSubmarine()
+	sub.RelayOrdersToHelmsman(orders)
+
+	result := sub.CurrentPosition()
+	assert.Equal(t, 1956047400, result)
 }
