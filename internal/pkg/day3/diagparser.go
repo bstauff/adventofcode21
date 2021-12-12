bd7c222a463b31a90a6e0edfa9da5bb765f0d0b1
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2021-12-11 22:42:18 -0600

    working on part 2

--- internal/pkg/day3/diagparser.go
+++ internal/pkg/day3/diagparser.go
@@ -1,6 +1,74 @@
 package day3
 
-func GetPowerConsumption(diagReport []uint16) uint64 {
+func GetPowerConsumption(diagReport []uint16, resultBitMask uint16) uint64 {
+	gamma := getMostCommonBits(diagReport)
+	epsilon := uint16(^gamma)
+	epsilon = epsilon & resultBitMask
+
+	powerRating := uint64(gamma) * uint64(epsilon)
+
+	return powerRating
+}
+
+func GetLifeSupportRating(diagReport []uint16, resultBitMask uint16) uint64 {
+	mostCommonBits := getMostCommonBits(diagReport)
+
+	masks := []uint16{
+		// 0x800,
+		// 0x400,
+		// 0x200,
+		// 0x100,
+		// 0x80,
+		// 0x40,
+		// 0x20,
+		0x10,
+		0x8,
+		0x4,
+		0x2,
+		0x1,
+	}
+	trimmed := make([]uint16, 0, len(diagReport))
+	copy(trimmed, diagReport)
+	for _, mask := range masks {
+		
+	}
+
+
+
+	oxygenRating := recursiveFilter(mostCommonBits, masks, 0, diagReport)
+
+	return uint64(oxygenRating[0])
+}
+
+func recursiveFilter(commonBits uint16, masks []uint16, maskIndex int, diagReports []uint16) []uint16 {
+	if len(diagReports) == 1 || maskIndex == len(masks) {
+		return diagReports
+	}
+
+	trimmed := filterWithMask(masks[maskIndex], diagReports, commonBits)
+
+	return recursiveFilter(commonBits, masks, maskIndex + 1, trimmed)
+}
+
+
+
+
+func filterWithMask(bitmask uint16, diagReports []uint16, commonBits uint16) []uint16 {
+	trimmedReports := make([]uint16, 0, len(diagReports))
+
+	for _, report := range diagReports {
+		bitvalue := report & bitmask
+
+		shouldGrabReport := bitvalue^(commonBits&bitmask) == 0
+
+		if shouldGrabReport {
+			trimmedReports = append(trimmedReports, report)
+		}
+	}
+	return trimmedReports
+}
+
+func getMostCommonBits(diagReport []uint16) uint16 {
 	masks := []uint16{
 		0x800,
 		0x400,
@@ -15,7 +83,6 @@ func GetPowerConsumption(diagReport []uint16) uint64 {
 		0x2,
 		0x1,
 	}
-
 	numberOfEntries := len(diagReport)
 	bitCounts := make([]int, 12)
 
@@ -27,18 +94,13 @@ func GetPowerConsumption(diagReport []uint16) uint64 {
 		}
 	}
 
-	gamma := uint16(0)
+	mostCommonBits := uint16(0)
 
 	for i, countOfOnes := range bitCounts {
 		if countOfOnes > numberOfEntries-countOfOnes {
-			gamma = gamma | masks[i]
+			mostCommonBits = mostCommonBits | masks[i]
 		}
 	}
 
-	epsilon := uint16(^gamma)
-	epsilon = epsilon & 0b0000111111111111
-
-	powerRating := uint64(gamma) * uint64(epsilon)
-
-	return powerRating
+	return mostCommonBits
 }
--- internal/pkg/day3/diagparser_test.go
+++ internal/pkg/day3/diagparser_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseHasGoodResults(t *testing.T) {
+func TestGetPowerConsumptionHasGoodResultsWithLargeInput(t *testing.T) {
 	transformFunc := func(s string) (interface{}, error) {
 		parsedBytes, err := strconv.ParseUint(s, 2, 16)
 
@@ -27,7 +27,49 @@ func TestParseHasGoodResults(t *testing.T) {
 	for i, data := range input {
 		castInput[i] = uint16(data.(uint64))
 	}
-	powerConsumption := GetPowerConsumption(castInput)
+	powerConsumption := GetPowerConsumption(castInput, 0b0000111111111111)
 
 	assert.Equal(t, uint64(2261546), powerConsumption)
+}
+
+func TestGetPowerConsumptionHasGoodResults(t *testing.T) {
+	input := []uint16 {
+		0b00100,
+		0b11110,
+		0b10110,
+		0b10111,
+		0b10101,
+		0b01111,
+		0b00111,
+		0b11100,
+		0b10000,
+		0b11001,
+		0b00010,
+		0b01010,
+	}
+
+	powerConsumption := GetPowerConsumption(input, 0b0000000000011111)
+
+	assert.Equal(t, uint64(198), powerConsumption)
+}
+
+func TestGetLifeSupportRatingHasGoodResults(t *testing.T) {
+	input := []uint16 {
+		0b00100,
+		0b11110,
+		0b10110,
+		0b10111,
+		0b10101,
+		0b01111,
+		0b00111,
+		0b11100,
+		0b10000,
+		0b11001,
+		0b00010,
+		0b01010,
+	}
+
+	lifeSupportRating := GetLifeSupportRating(input, 0b0000000000011111)
+
+	assert.Equal(t, uint64(230), lifeSupportRating)
 }
